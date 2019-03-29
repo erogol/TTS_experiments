8444b296af3b1ecdcbd5af2a172d865ed8e96dc5
Eren Golge <egolge@mozilla.com>
2019-03-29 17:39:26 +0100

    Remove BN Prenet and use as in the paper

--- layers/tacotron2.py
+++ layers/tacotron2.py
@@ -53,17 +53,16 @@ class LinearBN(nn.Module):
 
 
 class Prenet(nn.Module):
-    def __init__(self, in_features, out_features=[256, 256]):
+    def __init__(self, in_dim, sizes):
         super(Prenet, self).__init__()
-        in_features = [in_features] + out_features[:-1]
-        self.layers = nn.ModuleList([
-            LinearBN(in_size, out_size, bias=False)
-            for (in_size, out_size) in zip(in_features, out_features)
-        ])
+        in_sizes = [in_dim] + sizes[:-1]
+        self.layers = nn.ModuleList(
+            [Linear(in_size, out_size, bias=False)
+             for (in_size, out_size) in zip(in_sizes, sizes)])
 
     def forward(self, x):
         for linear in self.layers:
-            x = F.relu(linear(x))
+            x = F.dropout(F.relu(linear(x)), p=0.5, training=True)
         return x
         
 
