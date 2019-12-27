633b301bca0ab842a0f1bef760a0e5cfe4416306
root <root@sp-mlc3-5423-0.mlc>
2019-12-27 13:31:51 +0000

    move multi head attention to common layers

--- layers/gst_layers.py
+++ layers/gst_layers.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from TTS.layers.common_layers import MultiHeadAttention
+
 
 class GST(nn.Module):
     """Global Style Token Module for factorizing prosody in speech.
@@ -114,56 +116,3 @@ class StyleTokenLayer(nn.Module):
         style_embed = self.attention(prosody_encoding, tokens)
 
         return style_embed
-
-
-class MultiHeadAttention(nn.Module):
-    '''
-    input:
-        query --- [N, T_q, query_dim]
-        key --- [N, T_k, key_dim]
-    output:
-        out --- [N, T_q, num_units]
-    '''
-
-    def __init__(self, query_dim, key_dim, num_units, num_heads):
-
-        super().__init__()
-        self.num_units = num_units
-        self.num_heads = num_heads
-        self.key_dim = key_dim
-
-        self.W_query = nn.Linear(
-            in_features=query_dim, out_features=num_units, bias=False)
-        self.W_key = nn.Linear(
-            in_features=key_dim, out_features=num_units, bias=False)
-        self.W_value = nn.Linear(
-            in_features=key_dim, out_features=num_units, bias=False)
-
-    def forward(self, query, key):
-        queries = self.W_query(query)  # [N, T_q, num_units]
-        keys = self.W_key(key)  # [N, T_k, num_units]
-        values = self.W_value(key)
-
-        split_size = self.num_units // self.num_heads
-        queries = torch.stack(
-            torch.split(queries, split_size, dim=2),
-            dim=0)  # [h, N, T_q, num_units/h]
-        keys = torch.stack(
-            torch.split(keys, split_size, dim=2),
-            dim=0)  # [h, N, T_k, num_units/h]
-        values = torch.stack(
-            torch.split(values, split_size, dim=2),
-            dim=0)  # [h, N, T_k, num_units/h]
-
-        # score = softmax(QK^T / (d_k ** 0.5))
-        scores = torch.matmul(queries, keys.transpose(2, 3))  # [h, N, T_q, T_k]
-        scores = scores / (self.key_dim**0.5)
-        scores = F.softmax(scores, dim=3)
-
-        # out = score * V
-        out = torch.matmul(scores, values)  # [h, N, T_q, num_units/h]
-        out = torch.cat(
-            torch.split(out, 1, dim=0),
-            dim=3).squeeze(0)  # [N, T_q, num_units]
-
-        return out
