1f466fe321f6c4752b758f55a230f91754a06569
root <root@sp-mlc3-5423-0.mlc>
2020-01-03 00:16:50 +0100

    new measure to check attention

--- utils/measures.py
+++ utils/measures.py
@@ -1,11 +1,23 @@
+import numpy as np
 
-def alignment_diagonal_score(alignments):
+
+def alignment_score(alignments):
     """
-    Compute how diagonal alignment predictions are. It is useful
-    to measure the alignment consistency of a model
+    Measures how certain is the attention module.
     Args:
         alignments (torch.Tensor): batch of alignments.
     Shape:
         alignments : batch x decoder_steps x encoder_steps
     """
     return alignments.max(dim=1)[0].mean(dim=1).mean(dim=0).item()
+
+
+def alignment_diagonal_score(alignments):
+    """
+    Distance of the given alignment matrix to diagonal alignment.
+    Args:
+        alignments (torch.Tensor): batch of alignments.
+    Shape:
+        alignments : batch x decoder_steps x encoder_steps
+    """
+    return abs(np.array(range(alignments.shape[1])) - alignments.argmax(dim=2)[0].numpy()).sum() / alignments.shape[1]
