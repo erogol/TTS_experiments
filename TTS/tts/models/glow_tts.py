75ce37ecb58765a1bb166cb6d3687e5ced72a074
erogol <erogol@hotmail.com>
2020-07-30 10:52:52 +0200

    update mas related functions

--- TTS/tts/models/glow_tts.py
+++ TTS/tts/models/glow_tts.py
@@ -6,33 +6,7 @@ from torch.nn import functional as F
 from TTS.tts.layers.glow_tts.encoder import Encoder
 from TTS.tts.layers.glow_tts.decoder import Decoder
 from TTS.tts.utils.generic_utils import sequence_mask
-from TTS.tts.layers.glow_tts.monotonic_align import maximum_path
-
-
-def convert_pad_shape(pad_shape):
-    l = pad_shape[::-1]
-    pad_shape = [item for sublist in l for item in sublist]
-    return pad_shape
-
-
-def generate_path(duration, mask):
-    """
-    duration: [b, t_x]
-    mask: [b, t_x, t_y]
-    """
-    device = duration.device
-
-    b, t_x, t_y = mask.shape
-    cum_duration = torch.cumsum(duration, 1)
-    path = torch.zeros(b, t_x, t_y, dtype=mask.dtype).to(device=device)
-
-    cum_duration_flat = cum_duration.view(b * t_x)
-    path = sequence_mask(cum_duration_flat, t_y).to(mask.dtype)
-    path = path.view(b, t_x, t_y)
-    path = path - F.pad(path, convert_pad_shape([[0, 0], [1, 0], [0, 0]
-                                                 ]))[:, :-1]
-    path = path * mask
-    return path
+from TTS.tts.layers.glow_tts.monotonic_align import maximum_path, generate_path
 
 
 class GlowTts(nn.Module):
