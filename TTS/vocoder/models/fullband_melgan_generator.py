0395818f031337f0987140359f570cb938b1633e
erogol <erogol@hotmail.com>
2020-09-15 11:54:12 +0200

    fix fullband melgan

--- TTS/vocoder/models/fullband_melgan_generator.py
+++ TTS/vocoder/models/fullband_melgan_generator.py
@@ -9,10 +9,10 @@ class FullbandMelganGenerator(MelganGenerator):
                  in_channels=80,
                  out_channels=4,
                  proj_kernel=7,
-                 base_channels=384,
+                 base_channels=512,
                  upsample_factors=(2, 8, 2, 2),
                  res_kernel=3,
-                 num_res_blocks=3):
+                 num_res_blocks=4):
         super(FullbandMelganGenerator,
               self).__init__(in_channels=in_channels,
                              out_channels=out_channels,
@@ -21,13 +21,6 @@ class FullbandMelganGenerator(MelganGenerator):
                              upsample_factors=upsample_factors,
                              res_kernel=res_kernel,
                              num_res_blocks=num_res_blocks)
-        self.pqmf_layer = PQMF(N=4, taps=62, cutoff=0.15, beta=9.0)
-
-    def pqmf_analysis(self, x):
-        return self.pqmf_layer.analysis(x)
-
-    def pqmf_synthesis(self, x):
-        return self.pqmf_layer.synthesis(x)
 
     @torch.no_grad()
     def inference(self, cond_features):
@@ -36,4 +29,4 @@ class FullbandMelganGenerator(MelganGenerator):
             cond_features,
             (self.inference_padding, self.inference_padding),
             'replicate')
-        return self.pqmf_synthesis(self.layers(cond_features))
+        return self.layers(cond_features)
